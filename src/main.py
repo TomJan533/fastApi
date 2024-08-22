48ab97a2f0c87874abdf1c90d3ce5d94632c5608
TomJan533 <tomaszjan533@gmail.com>
2024-08-22 15:36:24 +0200

    documentation update

--- src/main.py
+++ src/main.py
@@ -4,8 +4,8 @@ from fastapi.responses import RedirectResponse
 from src.routers import example
 
 app = FastAPI(
-    title="RAG Chat Api",
-    description="This is a Retrieval Augumented Generation API built with FastAPI.",
+    title="FastAPI base project",
+    description="This is a base project built with FastAPI.",
     version="1.0.0",
     contact={
         "name": "tomaszj",
